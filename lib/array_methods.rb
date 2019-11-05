d4024d1bd15643f4d6b78548ea913bc7caea709b
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-05 22:54:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,19 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.sort
+  sorted_array=array.sort
+  p sorted_array
 end
 
 def using_reverse(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.reverse
+  sorted_array=array.reverse
+  p sorted_array
 end
 
 def using_first(array)
